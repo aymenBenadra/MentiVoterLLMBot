957a9c0ed9eb7ca4ee50eaf694689397e02accdb
Vishwaraj Anand <vishwarajanand@fb.com>
2021-06-20 19:25:04 +0800

    Adding a sample secrets file for reproducibility

---
+++ secrets.py
@@ -0,0 +1,3 @@
+menti_code='123456'
+# Chrome -> Network Conditions -> Chrome Mac
+useragent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36'
